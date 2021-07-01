68f11b98a2b4ad640bd1d7d425bc258d1804a072
hunzelrusher <71820619+hunzelrusher@users.noreply.github.com>
2021-07-01 14:45:32 +0200

    noch mehr Wolken

--- src/preload.js
+++ src/preload.js
@@ -13,6 +13,9 @@ class Preload extends Phaser.Scene {
         this.load.image("c3","./img/background/cloud3.png");
         this.load.image("c4","./img/background/cloud4.png");
         this.load.image("c5","./img/background/cloud5.png");
+        this.load.image("c6","./img/background/cloud6.png");
+        this.load.image("c7","./img/background/cloud7.png");
+        this.load.image("c8","./img/background/cloud8.png");
 
         this.load.image("dirt", "./img/platform/DirtTiles16.png");
         this.load.tilemapTiledJSON("dirtmap","./img/platform/dirtmap.json");
--- src/scene1.js
+++ src/scene1.js
@@ -5,22 +5,17 @@ class Scene1 extends Phaser.Scene {
 
     create() {
         //hintergrund
-        this.b1 = this.add.tileSprite(0,0, config.width, config.height, "b1");
-        this.b1.setOrigin(0, 0);
-        this.b2 = this.add.tileSprite(0,0, config.width, config.height, "b2");
-        this.b2.setOrigin(0, 0);
-        this.b3 = this.add.tileSprite(0,0, config.width, config.height, "b3");
-        this.b3.setOrigin(0, 0);
-        this.c1 = this.add.tileSprite(0,0, config.width, config.height, "c1");
-        this.c1.setOrigin(0, 0);
-        this.c2 = this.add.tileSprite(0,0, config.width, config.height, "c2");
-        this.c2.setOrigin(0, 0);
-        this.c3 = this.add.tileSprite(0,0, config.width, config.height, "c3");
-        this.c3.setOrigin(0, 0);
-        this.c4 = this.add.tileSprite(0,0, config.width, config.height, "c4");
-        this.c4.setOrigin(0, 0);
-        this.c5 = this.add.tileSprite(0,0, config.width, config.height, "c5");
-        this.c5.setOrigin(0, 0);
+        this.b1 = this.add.tileSprite(0,0, config.width, config.height, "b1").setOrigin(0, 0);
+        this.b2 = this.add.tileSprite(0,0, config.width, config.height, "b2").setOrigin(0, 0);
+        this.b3 = this.add.tileSprite(0,0, config.width, config.height, "b3").setOrigin(0, 0);
+        this.c1 = this.add.tileSprite(0,0, config.width, config.height, "c1").setOrigin(0, 0);
+        this.c2 = this.add.tileSprite(0,0, config.width, config.height, "c2").setOrigin(0, 0);
+        this.c3 = this.add.tileSprite(0,0, config.width, config.height, "c3").setOrigin(0, 0);
+        this.c4 = this.add.tileSprite(0,0, config.width, config.height, "c4").setOrigin(0, 0);
+        this.c5 = this.add.tileSprite(0,0, config.width, config.height, "c5").setOrigin(0, 0);
+        this.c6 = this.add.tileSprite(0,0, config.width, config.height, "c6").setOrigin(0, 0);
+        this.c7 = this.add.tileSprite(0,0, config.width, config.height, "c7").setOrigin(0, 0);
+        this.c8 = this.add.tileSprite(0,0, config.width, config.height, "c8").setOrigin(0, 0);
 
         //platformen
         const map = this.make.tilemap({key: 'dirtmap'});
@@ -41,10 +36,13 @@ class Scene1 extends Phaser.Scene {
     }
 
     moveClouds(){
-        this.c1.tilePositionX -= 0.1;
-        this.c2.tilePositionX -= 0.3;
-        this.c3.tilePositionX -= 0.1;
-        this.c4.tilePositionX -= 0.3;
-        this.c5.tilePositionX -= 0.2;
+        this.c1.tilePositionX -= 0.03;
+        this.c2.tilePositionX -= 0.2;
+        this.c3.tilePositionX -= 0.08;
+        this.c4.tilePositionX -= 0.21;
+        this.c5.tilePositionX -= 0.09;
+        this.c6.tilePositionX -= 0.16;
+        this.c7.tilePositionX -= 0.18;
+        this.c8.tilePositionX -= 0.09;
     }
 }
