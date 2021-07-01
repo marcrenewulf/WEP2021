2c51e12d944e86eda68aa639cdde8033bdf03b2a
hunzelrusher <71820619+hunzelrusher@users.noreply.github.com>
2021-07-01 13:55:30 +0200

    Wolken zugefügt

--- src/preload.js
+++ src/preload.js
@@ -4,6 +4,15 @@ class Preload extends Phaser.Scene {
     }
 
     preload() {
+        //hintergund
+        this.load.image("b1","./img/background/background1.png");
+        this.load.image("b2","./img/background/background2.png");
+        this.load.image("b3","./img/background/background3.png");
+        this.load.image("c1","./img/background/cloud1.png");
+        this.load.image("c2","./img/background/cloud2.png");
+        this.load.image("c3","./img/background/cloud3.png");
+        this.load.image("c4","./img/background/cloud4.png");
+        this.load.image("c5","./img/background/cloud5.png");
 
         this.load.image("dirt", "./img/platform/DirtTiles16.png");
         this.load.tilemapTiledJSON("dirtmap","./img/platform/dirtmap.json");
--- src/scene1.js
+++ src/scene1.js
@@ -4,15 +4,44 @@ class Scene1 extends Phaser.Scene {
     }
 
     create() {
-        {
-            const map = this.make.tilemap({key: 'dirtmap'});
-            const tileset = map.addTilesetImage('DirtTiles16','dirt');
-            const platforms = map.createLayer('Platforms', tileset);
-            platforms.setCollisionByProperty({collide: true});
-        }
+        //hintergrund und platformen erstellen
+        this.b1 = this.add.tileSprite(0,0, config.width, config.height, "b1");
+        this.b1.setOrigin(0, 0);
+        this.b2 = this.add.tileSprite(0,0, config.width, config.height, "b2");
+        this.b2.setOrigin(0, 0);
+        this.b3 = this.add.tileSprite(0,0, config.width, config.height, "b3");
+        this.b3.setOrigin(0, 0);
+        this.c1 = this.add.tileSprite(0,0, config.width, config.height, "c1");
+        this.c1.setOrigin(0, 0);
+        this.c2 = this.add.tileSprite(0,0, config.width, config.height, "c2");
+        this.c2.setOrigin(0, 0);
+        this.c3 = this.add.tileSprite(0,0, config.width, config.height, "c3");
+        this.c3.setOrigin(0, 0);
+        this.c4 = this.add.tileSprite(0,0, config.width, config.height, "c4");
+        this.c4.setOrigin(0, 0);
+        this.c5 = this.add.tileSprite(0,0, config.width, config.height, "c5");
+        this.c5.setOrigin(0, 0);
+
+
+
+
+
+
+        const map = this.make.tilemap({key: 'dirtmap'});
+        const tileset = map.addTilesetImage('DirtTiles16','dirt');
+        const platforms = map.createLayer('Platforms', tileset);
+        platforms.setCollisionByProperty({collide: true});
     }
 
     update() {
+        this.moveClouds();
+    }
 
+    moveClouds(){
+        this.c1.tilePositionX -= 0.1;
+        this.c2.tilePositionX -= 0.3;
+        this.c3.tilePositionX -= 0.1;
+        this.c4.tilePositionX -= 0.3;
+        this.c5.tilePositionX -= 0.2;
     }
 }
